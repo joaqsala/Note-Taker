04ba6fceee58e66e15c9361d9a725242df0d20f3
Joaquin Salazar <joaqsala@gmail.com>
2023-04-21 14:08:32 -0500

    corrected file structure

--- Develop/server.js
+++ Develop/server.js
@@ -2,17 +2,19 @@ const express = require('express');
 const path = require('path');
 const taskList = require('./Develop/db/db.json')
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3003;
 
 const app = express();
 
-//Middleware to retrieve posted data
+//Middleware for express to retrieve posted data and handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'))
 
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 
 app.listen(PORT, () => {
