9a2153c254bdf964d3b8b44ea8d1efd03f3f171a
Joaquin Salazar <joaqsala@gmail.com>
2023-04-22 20:10:50 -0500

    added wildcard route

--- Develop/middleware/reroute.js
+++
@@ -1,1 +0,0 @@
-//I made this file, will probably be needed when clicking on the Note-taking link at the top of the page
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+
 const taskList = require('./db/db.json')
 
 const PORT = process.env.port || 3003;
@@ -11,17 +11,25 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//used to publish contents of the static folder
 app.use(express.static('public'))
 
+// GET Route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// GET Route for notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => res.json((taskList)));
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 
 app.listen(PORT, () => {
