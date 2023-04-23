12ca877cca8b6ed984595eab7ad0272a268d0018
Joaquin Salazar <joaqsala@gmail.com>
2023-04-23 11:11:56 -0500

    added post route

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -1,24 +1,25 @@
 const fs = require('fs');
 
-
+const readAndAppend = (newNote) => {
 fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) {
-      console.error(err);
+        console.error(err);
     } else {
       // Convert string into JSON object
-      const parsedNotes = JSON.parse(data);
+        const parsedNotes = JSON.parse(data);
 
-      // Add a new review
-      parsedReviews.push(newReview);
+      // Add a new note
+        parsedNotes.push(newNote);
 
-      // Write updated reviews back to the file
-      fs.writeFile(
-        './db/reviews.json',
-        JSON.stringify(parsedReviews, null, 4),
-        (writeErr) =>
-          writeErr
+      // Write updated notes back to the file
+        fs.writeFile(
+        './db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) =>
+            writeErr
             ? console.error(writeErr)
             : console.info('Successfully updated reviews!')
-      );
+        );
     }
-  });
+    });
+};
+
+module.exports = readAndAppend;
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const taskList = require('../db/db.json')
+const taskList = require('../db/db.json');
+const readAndAppend = require('../helpers/fsUtils');
 
 
 // GET Route for retrieving all the notes
@@ -9,6 +10,32 @@ router.get('/', (req, res) => {
     res.status(200).json((taskList));
 });
 
+
+// POST Route for adding a note
+router.post('/', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+        const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote);
+
+    const response = {
+        status: 'success',
+        body: newNote,
+    };
+
+    } else {
+        res.json('Error in posting note');
+    }
+});
 
 
 
